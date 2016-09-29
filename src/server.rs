aae4733f355041074a115f51e6f9efcdb221aaca
Tara Vancil <tbvanc@gmail.com>
2016-09-29 16:45:19 -0400

    add trailing comma

--- src/server.rs
+++ src/server.rs
@@ -1,7 +1,7 @@
 pub struct Server {
     pub currentTerm: u64, // Latest term the server has seen. 0 on boot
     pub votedFor: u32, // ID of leader the server voted for
-    pub log: [LogEntry]
+    pub log: [LogEntry],
 }
 
 enum ServerStatus {
