c28dd12fe54f35fcddd0f6b3534b5760d876d1c6
Tara Vancil <tbvanc@gmail.com>
2016-09-29 17:04:15 -0400

    add other fields to Server struct

--- src/server.rs
+++ src/server.rs
@@ -1,7 +1,28 @@
 pub struct Server {
-    pub currentTerm: u64, // Latest term the server has seen. 0 on boot
-    pub votedFor: u32, // ID of leader the server voted for
+    // persistent state
+
+    // latest term the server has seen
+    pub currentTerm: u64,
+    // candidateID that received vote in the current term
+    pub votedFor: u32,
+    // log entries which contain commands for state machine
     pub log: [LogEntry],
+
+    // Volatile state on all servers
+
+    // index of highest log entry known to be committed
+    pub commitIndex: u64,
+    // index of highest log entry applied to the state machine
+    pub lastApplied: u64,
+
+    // volatile state on leaders only
+
+    // for each server, index of next log entry to send
+    // initialized to leader's last log index + 1
+    pub nextIndex: [u64],
+    // for each server index of higest log entry known to be
+    // replicated on the server
+    pub matchIndex: [u64],
 }
 
 enum ServerStatus {
