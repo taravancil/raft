d386c3fc9a290759616a78d7978b9d1dc60e63e4
Tara Vancil <tbvanc@gmail.com>
2016-09-30 11:08:02 -0400

    implement new and append for Raft client struct

--- src/lib.rs
+++ src/lib.rs
@@ -2,3 +2,13 @@ pub struct Raft {
     leader: u64, // the ID of the current leader
     servers: [Server], // the servers in the cluster
 }
+
+impl Raft {
+    /// Create a new `Raft` client
+    pub fn new() -> Raft {
+    }
+
+    /// Append an entry to the cluster's state machine
+    pub fn append() -> Result<()> {
+    }
+}
